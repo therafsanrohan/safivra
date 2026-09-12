35a14e246f3ee25323ce81836f19569a92b50111
Rafsan Rohan <knock.rafsan@gmail.com>
2026-09-12 20:01:43 +0600

    fix bugs

--- apps/api/src/modules/real-wealth/real-wealth.module.ts
+++ apps/api/src/modules/real-wealth/real-wealth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RealWealthController } from './real-wealth.controller';
 import { RealWealthService } from './real-wealth.service';
 import { SupabaseModule } from '../../supabase/supabase.module';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, AuthModule],
   controllers: [RealWealthController],
   providers: [RealWealthService],
   exports: [RealWealthService],
